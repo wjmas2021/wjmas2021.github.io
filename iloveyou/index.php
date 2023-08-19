d3022e0afed51c7acd1bf012550faea7eade07db
wjmas <98438768+wjmas2021@users.noreply.github.com>
2023-08-19 10:24:38 +0800

    Add files via upload

---
+++ iloveyou/index.php
@@ -0,0 +1,189 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no" />
+    <link rel="icon" href="/favicon.ico">
+   <title> I LOVE YOU </title>
+   <meta name="keywords" content="表白网页" /> 
+   <meta name="description" content="恋爱纪念响应式优化版" /> 
+	 <script src="./js/js-sdk-perf.min.js" crossorigin="anonymous"></script><script>new LingQue.Monitor().init({id:"JWhH76zbSJ9K2QhV"});</script>    <link rel="stylesheet" type="text/css" href="./css/lovexhj.css">
+    <link rel="stylesheet" type="text/css" href="./css/swiper.css">
+    <link rel="stylesheet" type="text/css" href="./css/FiraCode.css">
+</head>
+
+<body>
+    <!-- 标题板块 -->
+    <div class="title">
+        <img src="./images/bg1.gif" alt="bgGif">
+        <p id="tqyxhj"></p>
+    </div>
+
+    <!-- 分页1 - 骚话 -->
+    <div class="page page1">
+        <div class="swiper-container">
+            <div class="swiper-wrapper" style="width:90%; margin:0 auto;">
+                <div class="swiper-slide">
+                    <div class="page1-lovexhj1">
+                        <img src="./images/Ha2f9396264034bdb8c34d519c7273819H.jfif" alt="左边的">
+                        <img src="./images/cat.gif" alt="cat">
+                        <img src="./images/download20220802072703.png" alt="右边的">
+                        <p id="lovetime"></p>
+						<br>
+                        <p>我也不知道该准备一点什么给你逗你开心 还是写一点好玩的送给你吧~</p>
+                    </div>
+                </div>
+
+            </div>
+
+        </div>
+    </div>
+
+    <!-- 分页2 - 时间轴 -->
+    <div class="page page2">
+        <div class="swiper-container swiper-container-lovexhj2">
+            <div class="swiper-wrapper">
+                <div class="swiper-slide">
+                    <div class="page2-box">
+                        <img src="./images/t0168b6fe7b1aa99a24.jpg" alt="page2-1">
+                        <p>这是我们爱情的开始，也是我们故事的开头</p>
+						<br>
+						<span style="font-size:14px;color:#ccc;">往右滑 → →</span>
+                    </div>
+                </div>
+                <div class="swiper-slide">
+                    <div class="page2-box">
+                        <img src="./images/t0109082874b6dc40e6.jpg" alt="page2-2">
+                        <p>自从遇到你之后，我就认定你是陪伴我一生的人。</p>
+						<span style="font-size:14px;color:#ccc;">后面还有哦 → →</span>
+						
+                    </div>
+                </div>
+                <div class="swiper-slide">
+                    <div class="page2-box">
+                        <img src="./images/Hfe8d4f1c618f44138fba7b1ad43d1e3fy.jpg" alt="page2-3">
+                        <p>难以忘记初次见你的感觉，我相信这就是一见钟情!！</p>
+						<span style="font-size:14px;color:#ccc;">最后一张啦 → →</span>
+                    </div>
+                </div>
+
+            </div>
+            <div class="swiper-pagination swiper-pagination-lovexhj2"></div>
+        </div>
+    </div>
+
+    <!-- 分页3 - 一段话 -->
+    <div class="page page3">
+        <div id="talkToXHJ" style="text-align:center;">
+            <b>真情告白</b>
+            <br />
+           <p>
+		   世界60多亿人口,我们相遇的概率小到忽略不计.在有生之年遇见你,花光了我所有的运气.猪宝宝，三生有幸遇到你。           
+		   </p> 
+            
+        </div>
+    </div>
+
+    <!-- 分页4 - 结尾 -->
+    <div class="page page4">
+        <div class="page4-box">
+            <div class="page4-img">
+                <div class="swiper-container swiper-container-lovexhj3">
+                    <div class="swiper-wrapper">
+					
+                        <div class="swiper-slide">
+                            <img src="./images/t0109082874b6dc40e6.jpg" alt="page-4-1">
+                        </div>
+						
+                        <div class="swiper-slide">
+                            <img src="./images/t0168b6fe7b1aa99a24.jpg" alt="page-4-2">
+                        </div>
+						
+                        <div class="swiper-slide">
+                            <img src="./images/t012620fb21ab954ad5.jpg" alt="page-4-3">
+                        </div>
+
+                    </div>
+                    <div class="swiper-button-prev swiper-button-prev-lovexhj3"></div>
+                    <div class="swiper-button-next swiper-button-next-lovexhj3"></div>
+                </div>
+            </div>
+			
+            <div class="page4-text">
+                <p style="text-align:center;">如果可以，我想陪你很久很久。</p>
+            </div>
+        </div>
+
+
+    </div>
+
+
+    <div class="xhjIsPig">
+        <img src="./images/pig1.gif" alt="pig1" class="pig1">
+        <img src="./images/pig2.gif" alt="pig2" class="pig2">
+    </div>
+
+ <script>var _bgm_url = "./audio/e2121b11ad19cbfd0962e944fe47ec96.mp3";</script><script src="./js/music_play.js"></script>    <!-- js加载 -->
+	<script charset="UTF-8" id="LA_COLLECT" src="./js/js-sdk-pro.min.js"></script><script>LA.init({id: "JWhHl05vnbv6HXOZ",ck: "JWhHl05vnbv6HXOZ"})</script> 
+	
+	
+	<script>var zaiyiqi = '2023-08-12 00:00:00';</script>
+    <script src="./js/wow.js"></script>
+    <script src="./js/typeit.min.js"></script>
+    <script src="./js/swiper-3.4.2.min.js"></script>
+    <script src="./js/jquery.js"></script>
+
+    <script src="./js/lovetime.js"></script>
+    <script src="./js/swiper.js"></script>
+    <script src="./js/su.js"></script>
+    <script>
+        (function () {
+            new WOW().init()
+        })()
+    </script>
+
+    <script type="text/javascript">
+	 pageOneCont("WJMAS && HUTAO","此网页送给最可爱的程序猿")
+	function pageOneCont(str1,str2){
+		new TypeIt("#tqyxhj", {
+					loop: true,
+					cursorSpeed: 1000,
+					speed: 100
+				})
+				.type(str1)
+				.pause(2000)
+				.delete(null, {
+					delay: 500
+				})
+				.type(str2)
+				.pause(3000)
+				.go();
+
+			new TypeIt('#talkToXHJ', {
+				lifeLike: true,
+				cursorSpeed: 1000,
+				waitUntilVisible: true,
+				speed: 100
+			}).go();
+	}
+			
+			
+			
+	
+        jQuery(document).ready(function ($) {
+            $('body').wpSuperSnow({
+                flakes: ['./images/004.png', './images/003.png', './images/002.png',
+                    './images/001.png'
+                ],
+                totalFlakes: '100',
+                zIndex: '999999',
+                maxSize: '30',
+                maxDuration: '20',
+                useFlakeTrans: false
+            });
+        });
+    </script>
+</body>
+
+</html>
